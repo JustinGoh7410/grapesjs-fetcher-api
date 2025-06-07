211b2a1bdfc49408db3c779e1bdb03218f602196
Justin Goh <justin.goh@oom.com.sg>
2025-06-07 22:55:21 +0800

    update again

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 async function fetchPageAndCss(url) {
   const browser = await puppeteer.launch({
     headless: 'new',
-    executablePath: '/opt/render/.cache/puppeteer/chrome/linux-137.0.7151.55/chrome-linux64/chrome',
+    executablePath: puppeteer.executablePath(),
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
